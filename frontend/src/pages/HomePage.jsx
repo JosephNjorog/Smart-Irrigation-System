4565930e9e4532bbaae7d9ace59121dcf842f5ac
JosephNjorog <njojoe11173@gmail.com>
2024-10-05 20:24:00 +0300

    Add ArcGISMap component to display a map on the home page

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ArcGISMap from '../components/ArcGISMap';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Smart Irrigation and Crop Monitoring System</h1>
+      <ArcGISMap />
+    </div>
+  );
+};
+
+export default HomePage;

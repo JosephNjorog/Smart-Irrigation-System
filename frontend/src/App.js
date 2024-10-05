9eaab97da96d9a74fcc5447c45a213022c2d5321
JosephNjorog <njojoe11173@gmail.com>
2024-10-06 00:34:45 +0300

    Refactor routing in App.js to use Switch component for better performance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import OpenLayersMap from './components/OpenLayersMap';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import IrrigationPage from './pages/IrrigationPage';
 import CropHealthPage from './pages/CropHealthPage';
 import SensorDataPage from './pages/SensorDataPage';
-import './assets/styles/App.css';
+import Navbar from './components/Navbar';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/irrigation" component={IrrigationPage} />
-        <Route path="/crop-health" component={CropHealthPage} />
-        <Route path="/sensor-data" component={SensorDataPage} />
-      </Routes>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/irrigation" component={IrrigationPage} />
+          <Route path="/crop-health" component={CropHealthPage} />
+          <Route path="/sensor-data" component={SensorDataPage} />
+        </Switch>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
